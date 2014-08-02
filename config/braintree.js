090ad9de298fd095a95379ec85f03d37731bb678
James-Mac <james@aehr.org>
2014-08-02 14:06:44 -0600

    Adjustments to braintree setup

--- config/braintree.js
+++ config/braintree.js
@@ -2,10 +2,16 @@
   Braintree configuration
 */
 
-var mongoose = require('mongoose'),
-    winston  = require('winston');
+var mongoose        = require('mongoose'),
+    winston         = require('winston'),
+    braintree       = require('braintree'),
+    braintreeConfig = require('../config/keys').braintree;
 
 
-exports.init = function (config) {
+exports.init = function () {
   winston.info('Connecting to braintree...');
+  // Set Braintree Environment
+  braintreeConfig.environment = braintree.Environment.Sandbox;
+  // Return gateway
+  return braintree.connect(braintreeConfig);
 }
--- handlers/transaction.js
+++ handlers/transaction.js
@@ -1,13 +1,19 @@
-var braintreeConfig = require('../config/keys').braintree,
-    gateway         = require('./config/braintree').init(braintreeConfig);
+var gateway = require('../config/braintree').init(),
+    User    = require('../models/user');
 
 /*
   Method for generating Braintree client-side SDK token.
+
+  1. Will check for BT vault existance
+  2. Will register customer if not in BT vault
+  3. Will generate token with BT customer id
 
   -> GET /transaction/token
   <- Object > @status, @token
 */
 exports.generateBraintreeToken = function (req, res, next) {
+  var user = req.query.user;
+
   if(!user) {
     return res.status(500).json({
       status: 'error',
@@ -15,32 +21,74 @@ exports.generateBraintreeToken = function (req, res, next) {
     });
   }
 
-  var op = {
-    customerId: req.user
-  };
-
-  gateway.clientToken.generate(op, function (err, braintreeRes) {
-    if(err) {
+  findCustomer(user, function (err, customer) {
+    if(err && err.name !== 'notFoundError') {
       return res.status(500).json({
         status: 'error',
         error: err
       });
-    } else if(!braintreeRes.clientToken) {
-      return res.status(500).json({
-        status: 'error',
-        error: 'Unable to generate token'
+    } else if(!customer) {
+      return registerCustomer(user, function (err, newCustomer) {
+        if(err) {
+          return res.status(500).json({
+            status: 'error',
+            error: err
+          });
+        }
+
+        generateToken({
+          customerId: newCustomer.id
+        });
       });
     }
-
-    res.status(200).json({
-      status: 'ok',
-      token: braintreeRes.clientToken
+    generateToken({
+      customerId: user
     });
   });
+
+  var generateToken = function (op) {
+    gateway.clientToken.generate(op, function (err, braintreeRes) {
+      if(err) {
+        return res.status(500).json({
+          status: 'error',
+          error: err
+        });
+      } else if(!braintreeRes.clientToken) {
+        return res.status(500).json({
+          status: 'error',
+          error: 'Unable to generate token'
+        });
+      }
+
+      res.status(200).json({
+        status: 'ok',
+        token: braintreeRes.clientToken
+      });
+    });
+  }
 }
 
+
+/*
+  Private Methods
+*/
 function findCustomer (id, callback) {
-  gateway.customer.find(id, function(err, customer) {
-    callback(err, customer);
+  gateway.customer.find(id, callback);
+}
+
+function registerCustomer (id, callback) {
+  User.findById(id, function (err, user) {
+    if(err) {
+      return callback(err);
+    }
+
+    var braintreeCustomer = {
+      id: id,
+      firstName: user.name.first,
+      lastName: user.name.last,
+      email: user.login.email
+    };
+
+    gateway.customer.create(braintreeCustomer, callback);
   });
 }
