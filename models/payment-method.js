37dd4f290d1c1c36be277fe6b1e0c74c9c035e4f
James-Mac <james@aehr.org>
2014-08-08 20:49:58 -0600

    Payment method model

---
+++ models/payment-method.js
@@ -0,0 +1,31 @@
+/*
+  Payment Method - Server Data Model
+*/
+
+var mongoose =   require('mongoose'),
+    Schema =     mongoose.Schema,
+    momentDate = require('../utils/moment-date');
+
+var paymentMethodSchema = new Schema({
+  name:      String,
+  // Relational
+  customerId: { type : mongoose.Schema.ObjectId, ref : 'User' },
+
+  // Options
+  isDefault:  boolean,
+
+  // Customer Data
+  address: {
+    line1:   String,
+    line2:   String,
+    city:    String,
+    state:   String,
+    zipcode: String
+  },
+
+  // System
+  methodError: String,
+  time_stamp: { type: String, default: momentDate() }
+});
+
+module.exports = mongoose.model('PaymentMethod', paymentMethodSchema);
